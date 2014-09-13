05566fc996cf67c5d6ec141f8e5329f7e84effd8
Bondan Herumurti <bherumurti@gmail.com>
2014-09-14 00:14:49 +0700

    pantang menyerah

---
+++ config/unicorn/production.rb
@@ -0,0 +1,19 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/var/www/test_app/current"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid File.expand_path('tmp/pids/unicorn.pid', ENV['RAILS_ROOT']).to_s
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/test_app/current/log/unicorn.log"
+stdout_path "/var/www/test_app/current/log/unicorn.log"
+
+listen "/tmp/unicorn.[test_app].sock"
+listen "/tmp/unicorn.test_app.sock"
+worker_processes 2
+timeout 30
+preload_app true
